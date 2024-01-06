e653bae22ffb3f20acb700b9cbbacf5673234c38
AlamRidha <alamrridha@gmail.com>
2024-01-06 18:18:14 +0700

    Make 2 fragment

--- app/src/main/java/com/example/mobilefundamental/MainActivity.kt
+++ app/src/main/java/com/example/mobilefundamental/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.mobilefundamental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.mobilefundamental.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //  declaration fragment to overwrite mainactivity
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
 
+        if (fragment !is HomeFragment) {
+            Log.d(TAG, "Fragment name : " + HomeFragment::class.java.simpleName)
+            fragmentManager.beginTransaction()
+                .add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
+
+    }
+
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
